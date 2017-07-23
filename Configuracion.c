619f660e18c3980d00027728b1647500dba7c819
Enrique Ortega <EnriqM@users.noreply.github.com>
2017-07-23 18:55:09 +0200

    Create Configuracion.c

---
+++ Configuracion.c
@@ -0,0 +1,77 @@
+/////////////////////////////////////
+//  Generated Initialization File  //
+/////////////////////////////////////
+
+#include "C8051F340.h"
+
+// Peripheral specific initialization functions,
+// Called from the Init_Device() function
+void PCA_Init()
+{
+    PCA0MD    &= ~0x40;
+    PCA0MD    = 0x00;
+}
+
+void Timer_Init()
+{
+    TCON      = 0x01;
+}
+
+void Port_IO_Init()
+{
+    // P0.0  -  Skipped,     Open-Drain, Digital
+    // P0.1  -  Skipped,     Open-Drain, Digital
+    // P0.2  -  Skipped,     Open-Drain, Digital
+    // P0.3  -  Skipped,     Open-Drain, Digital
+    // P0.4  -  Skipped,     Open-Drain, Digital
+    // P0.5  -  Skipped,     Open-Drain, Digital
+    // P0.6  -  T0  (Timr0), Open-Drain, Digital
+    // P0.7  -  Unassigned,  Open-Drain, Digital
+
+    // P1.0  -  Unassigned,  Open-Drain, Digital
+    // P1.1  -  Unassigned,  Open-Drain, Digital
+    // P1.2  -  Unassigned,  Open-Drain, Digital
+    // P1.3  -  Unassigned,  Open-Drain, Digital
+    // P1.4  -  Unassigned,  Open-Drain, Digital
+    // P1.5  -  Unassigned,  Open-Drain, Digital
+    // P1.6  -  Unassigned,  Open-Drain, Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Open-Drain, Digital
+    // P2.1  -  Unassigned,  Open-Drain, Digital
+    // P2.2  -  Unassigned,  Open-Drain, Digital
+    // P2.3  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.5  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.1  -  Unassigned,  Open-Drain, Digital
+    // P3.2  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.4  -  Unassigned,  Open-Drain, Digital
+    // P3.5  -  Unassigned,  Open-Drain, Digital
+    // P3.6  -  Unassigned,  Open-Drain, Digital
+    // P3.7  -  Unassigned,  Open-Drain, Digital
+
+    P0MDOUT   = 0x43;
+    P0SKIP    = 0x3F;
+    XBR1      = 0x50;
+}
+
+void Interrupts_Init()
+{
+    IT01CF    = 0x10;
+    // IE        = 0x87;
+}
+
+// Initialization function for device,
+// Call Init_Device() from your main program
+void Init_Device(void)
+{
+    PCA_Init();
+    // Timer_Init();
+    Port_IO_Init();
+    Interrupts_Init();
+}
